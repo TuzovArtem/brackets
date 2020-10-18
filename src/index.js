52a2e6007f8aa65dd2fa7007937511054bd09ab8
Artem_T <tuzovartem@gmail.com>
2020-10-18 16:33:50 +0400

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let allBrackets = bracketsConfig.map( (b) => b.join("") );
+
+  for (let i = 0; i < allBrackets.length; i++) {
+   
+    if( str.includes(allBrackets[i]) ) {
+      str = str.replace(allBrackets[i], "");
+      i = -1;
+    }
+  }
+  
+  if(str){
+    return false;
+  }else{
+    return true;
+  }
 }
+
